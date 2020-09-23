c54d5310b4b49af631ba9c1db8845b04a552df91
a-mar-se <a.marquez.seco@hotmail.com>
2020-09-23 23:28:34 +0200

    The apple now can be eaten

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,84 @@
-console.log('Success!');
+// Initialize the program
+init = () => {
+  plotPika(x, y);
+  appearApple();
+};
 
-const width = 4;
-const widthSize = 100 / width + '%';
+document.addEventListener('DOMContentLoaded', init);
+
+// Create variables for the grid
+const width = 2;
 const height = width;
-
 const celCount = width * height;
-
+const widthSize = 100 / width + '%'; // Variable CSS
 const grido = document.querySelector('.grid');
 
+// Generates a grid with the dimensions provided
 for (let i = 0; i < celCount; i++) {
-  // console.log(i);
   const cell = document.createElement('div');
-  cell.innerText = i;
-
   cell.style.setProperty('--heightCSS', widthSize);
-
   grido.appendChild(cell);
 }
 
-window.addEventListener('keydown', handleKeyPress);
-
-function keyPressed(event) {
-  const keypressed = event.target;
-  console.log(keypressed);
-}
-
+// Creating position coordinates
 let y = 0;
 let x = 0;
 
+// Get the cells of the grid as an argument
 const display = Array.from(document.querySelectorAll('.grid > div'));
 
+// Showing Pikachu on the cell with coordinates (posx, posy)
+function plotPika(posx, posy) {
+  const celdilla = display[posy * width + posx];
+  celdilla.classList.add('pika');
+  // console.log('Pika Pika!');
+}
+
+let manzy = 0;
+let manzx = 0;
+function appearApple() {
+  function generateNumber() {
+    return Math.floor(Math.random() * width);
+  }
+  // Generate random coordinates for the apple
+  manzy = generateNumber();
+  manzx = generateNumber();
+  while (manzx === x && manzy === y) {
+    manzy = generateNumber();
+    manzx = generateNumber();
+  }
+  // Find the cell of those coordinates
+  const celdilla = display[manzy * width + manzx];
+
+  // Create new element to show on top of the field
+  const cell = document.createElement('p');
+  cell.classList.add('apple');
+  celdilla.appendChild(cell);
+
+  console.log(`Apple created at position ${manzx},${manzy}`);
+}
+
+function eatApple(x, y) {
+  // snakeGrows(tailCell);
+  appleDisappears(x, y);
+  appearApple();
+}
+
+function appleDisappears(posx, posy) {
+  const celdilla = display[posy * width + posx];
+
+  console.log(celdilla);
+  const cell = celdilla.querySelector('p');
+  cell.classList.remove('apple');
+  celdilla.removeChild(document.querySelector('p'));
+  console.log('apple dissappeared');
+  // plotPika();
+}
+
+function snakeGrows() {}
+
+// When pressing a key...
+window.addEventListener('keydown', handleKeyPress);
 function handleKeyPress(event) {
   const key = event.key; //
   // const { key } = event;  // crea una constante llamada
@@ -65,29 +114,15 @@ function handleKeyPress(event) {
     default:
       console.log('Not an arrow');
   }
-
+  checkIfEats(x, y);
+  console.log(x, y);
+  console.log(manzx, manzy);
   plotPika(x, y);
 }
 
-function plotPika(posx, posy) {
-  const celdilla = display[posy * width + posx];
-
-  celdilla.classList.add('pika');
-  console.log(celdilla);
+function checkIfEats(x, y) {
+  if (x === manzx && y === manzy) {
+    eatApple(x, y);
+    console.log('Appple eaten!');
+  }
 }
-
-function apareceManzana() {
-  const randomX = Math.floor(Math.random() * width);
-  const randomY = Math.floor(Math.random() * width);
-
-  const celdilla = display[randomY * width + randomX];
-
-  const cell = document.createElement('p');
-  cell.classList.add('apple');
-
-  celdilla.appendChild(cell);
-  console.log(cell);
-}
-
-plotPika(x, y);
-apareceManzana();
