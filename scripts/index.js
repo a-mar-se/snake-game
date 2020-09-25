ca7badc2897da7607e8e09d8ba570d98d5a3dffc
a-mar-se <a.marquez.seco@hotmail.com>
2020-09-25 14:38:42 +0200

    Crashing implemented and

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,8 @@ init = () => {
     // startButton.addEventListener('mouseover', initBackground);
   }
 
-  waitForStart();
+  // waitForStart();
+  initGame();
 };
 
 initGame = () => {
@@ -38,15 +39,56 @@ initGame = () => {
   intro();
 
   const startTimer = (event) => {
+    const renderTimerTime = () => {
+      deciSeconds = deciSeconds + 1;
+      // seconds = seconds + 0.1;
+      let timeString = ``;
+      if (deciSeconds === 10) {
+        seconds = seconds + 1;
+        deciSeconds = 0;
+        if (seconds === 60) {
+          seconds = 0;
+          minutes = minutes + 1;
+          if (minutes === 60) {
+            minutes = 0;
+            hours = hours + 1;
+          }
+        }
+      }
+      if (hours < 10) {
+        timeString = timeString.concat(`0${hours} `);
+      } else {
+        timeString = `${timeString}${hourse} `;
+      }
+
+      if (minutes < 10) {
+        timeString = `${timeString}:0${minutes} `;
+      } else {
+        timeString = `${timeString}:${minutes} `;
+      }
+
+      if (seconds < 10) {
+        timeString = timeString.concat(`:0${seconds} `);
+      } else {
+        timeString = timeString.concat(`:${seconds} `);
+      }
+
+      score = score + 1;
+      timer.innerText = timeString;
+      scorePanel.innerText = score;
+      moveObjects();
+    };
     intervalId = setInterval(renderTimerTime, 100);
   };
-  setTimeout(startTimer, 3000);
+  setTimeout(startTimer, 1000);
 };
 document.addEventListener('DOMContentLoaded', init);
 
 const startButton = document.querySelector('.startButton');
 const startButtonContainer = document.querySelector('.startButtonContainer');
 
+let playing = true;
+let lifes = 3;
 let score = 0;
 let seconds = 0;
 let minutes = 0;
@@ -60,48 +102,10 @@ let count = 0;
 let intervalId = null;
 
 let deciSeconds = 0;
-const renderTimerTime = () => {
-  deciSeconds = deciSeconds + 1;
-  // seconds = seconds + 0.1;
-  let timeString = ``;
-  if (deciSeconds === 10) {
-    seconds = seconds + 1;
-    deciSeconds = 0;
-    if (seconds === 60) {
-      seconds = 0;
-      minutes = minutes + 1;
-      if (minutes === 60) {
-        minutes = 0;
-        hours = hours + 1;
-      }
-    }
-  }
-  if (hours < 10) {
-    timeString = timeString.concat(`0${hours} `);
-  } else {
-    timeString = `${timeString}${hourse} `;
-  }
 
-  if (minutes < 10) {
-    timeString = `${timeString}:0${minutes} `;
-  } else {
-    timeString = `${timeString}:${minutes} `;
-  }
-
-  if (seconds < 10) {
-    timeString = timeString.concat(`:0${seconds} `);
-  } else {
-    timeString = timeString.concat(`:${seconds} `);
-  }
-
-  score = score + 1;
-  timer.innerText = timeString;
-  scorePanel.innerText = score;
-  moveObjects();
-};
-
+const directions = ['up', 'left', 'down', 'right'];
 let timeCount = 0;
-
+let appleScore = 0;
 let attackProgress = 0;
 function moveObjects() {
   function checkIfEats() {
@@ -138,44 +142,68 @@ function moveObjects() {
       console.log('Appple eaten!');
     }
   }
+  function recieveDamage() {
+    function changeToSafeDirection() {
+      moveSnake(directions[(directions.indexOf(direction) + 2) % 4]);
+
+      // plotSnake();
+      direction = directions[(directions.indexOf(direction) + 1) % 4];
+    }
+    changeToSafeDirection();
+    console.log('You lost 1 life!');
+    lifes = lifes - 1;
+    if (lifes <= 0) {
+      lifes = 0;
+      console.log('Game Over');
+      clearInterval(intervalId);
+    }
+    // update lifes
+    document.getElementById('lifes').innerText = lifes;
+  }
   function checkCrash() {
-    const checkBody = (elem) => {
-      if (elem === snakeHead) {
-        function gameOver() {
-          console.log('Game Over');
-        }
-        console.log(`crash in position ${elem}`);
-        gameOver();
+    const checkWall = (elem) => {
+      if (elem == snakeHead) {
+        console.log(`crash with the wall ${elem}`);
+        recieveDamage();
       }
     };
-
+    const checkBody = (elem) => {
+      if (snakePositions.indexOf(elem) !== snakePositions.length - 1) {
+        if (elem == snakeHead) {
+          console.log(`crash with itself at ${elem}`);
+          recieveDamage();
+        }
+      }
+    };
+    wallsPositions.forEach(checkWall);
+    console.log(snakePositions);
     snakePositions.forEach(checkBody);
-    wallsPositions.forEach(checkBody);
   }
+
   function moveSnake(direction) {
     switch (direction) {
       case 'up':
-        if (y > 1) {
+        if (y > 0) {
           y = y - 1;
         }
         break;
 
       case 'down':
-        if (y < width - 2) {
+        if (y < width - 1) {
           y = y + 1;
         }
         break;
       case 'right':
-        if (x < width - 2) {
+        if (x < width - 1) {
           x = x + 1;
         }
         break;
       case 'left':
-        if (x > 1) {
+        if (x > 0) {
           x = x - 1;
         }
       default:
-        console.log('Not an arrow');
+        break;
     }
   }
   timeCount = timeCount + 0.1;
@@ -190,56 +218,66 @@ function moveObjects() {
     checkIfEats();
   }
 
-  const sun = document.querySelector('.dodge');
-  const attackPanel = document.querySelector('#attackBar > div');
-  console.log(attackPanel);
-  if (attackProgress < 100) {
-    attackProgress = attackProgress + 1;
-  } else {
-    attackPanel.classList.add('full-bar');
-  }
-  const attackProgress2 = attackProgress + 1;
-  const attackProgressValue = `${attackProgress}%`;
-  const attackProgressValue2 = `${attackProgress2}%`;
+  // attack bar filling
+  function attackFill() {
+    const attackPanel = document.querySelector('#attackBar > div');
+    if (attackProgress < 100) {
+      attackProgress = attackProgress + 1;
+    } else {
+      attackPanel.classList.add('full-bar');
+    }
+    const attackProgress2 = attackProgress + 1;
+    const attackProgressValue = `${attackProgress}%`;
+    const attackProgressValue2 = `${attackProgress2}%`;
 
-  attackPanel.style.setProperty('--attackBar', attackProgressValue);
-  attackPanel.style.setProperty('--attackBar2', attackProgressValue2);
-  const sunnyPos =
-    100 *
-    Math.cos(((((seconds + 0.1 * deciSeconds) * 100) / 60) * Math.PI) / 180);
-  const sunnxPos =
-    100 *
-    Math.sin(((((seconds + 0.1 * deciSeconds) * 100) / 60) * Math.PI) / 180);
-  const sunx = `${sunnxPos}%`;
-  const shinex = `${
-    100 *
-      Math.sin(((((seconds + 0.1 * deciSeconds) * 100) / 60) * Math.PI) / 180) -
-    2.25
-  }%`;
-  const suny = `${sunnyPos}%`;
-  const shiney = `${
-    100 *
-      Math.cos(((((seconds + 0.1 * deciSeconds) * 100) / 60) * Math.PI) / 180) +
-    2.5 * (((seconds + 0.1 * deciSeconds) * 100) / 60)
-  }%`;
-  // const planetx = `${sunnyPos + 0}%`;
-  // const planety = `${sunnyPos}%`;
-  const planetx = `${sunnxPos - (sunnxPos / 100) * 30}%`;
-  const planety = `${sunnyPos - (sunnyPos / 100) * 30}%`;
-  sun.style.setProperty('--sunx', sunx);
-  sun.style.setProperty('--suny', suny);
-  sun.style.setProperty('--shinex', shinex);
-  sun.style.setProperty('--shiney', shiney);
-  sun.style.setProperty('--planetx', planetx);
-  sun.style.setProperty('--planety', planety);
-  // sun.style.setProperty('--sunx', );
-  // sun.style.setProperty('--suny', `${Math.cos(seconds * 60)}%`);
+    attackPanel.style.setProperty('--attackBar', attackProgressValue);
+    attackPanel.style.setProperty('--attackBar2', attackProgressValue2);
+  }
+  attackFill();
+
+  // Sun in the background moving
+  function sunMove() {
+    const sun = document.querySelector('.dodge');
+    const sunnyPos =
+      100 *
+      Math.cos(((((seconds + 0.1 * deciSeconds) * 100) / 60) * Math.PI) / 180);
+    const sunnxPos =
+      100 *
+      Math.sin(((((seconds + 0.1 * deciSeconds) * 100) / 60) * Math.PI) / 180);
+    const sunx = `${sunnxPos}%`;
+    const shinex = `${
+      100 *
+        Math.sin(
+          ((((seconds + 0.1 * deciSeconds) * 100) / 60) * Math.PI) / 180,
+        ) -
+      2.25
+    }%`;
+    const suny = `${sunnyPos}%`;
+    const shiney = `${
+      100 *
+        Math.cos(
+          ((((seconds + 0.1 * deciSeconds) * 100) / 60) * Math.PI) / 180,
+        ) +
+      2.5 * (((seconds + 0.1 * deciSeconds) * 100) / 60)
+    }%`;
+
+    const planetx = `${sunnxPos - (sunnxPos / 100) * 30}%`;
+    const planety = `${sunnyPos - (sunnyPos / 100) * 30}%`;
+    sun.style.setProperty('--sunx', sunx);
+    sun.style.setProperty('--suny', suny);
+    sun.style.setProperty('--shinex', shinex);
+    sun.style.setProperty('--shiney', shiney);
+    sun.style.setProperty('--planetx', planetx);
+    sun.style.setProperty('--planety', planety);
+  }
+  sunMove();
 }
 
+// Head and move direction
 let direction = 'up';
 
 // Create variables for the grid
-const width = 13;
+const width = 6;
 const mapWidth = width - 2;
 const height = width;
 const celCount = width * height;
@@ -265,7 +303,6 @@ createGrid();
 
 // Get the cells of the grid as an argument
 const display = Array.from(document.querySelectorAll('.grid > div>p'));
-console.log(display);
 
 // Creating position coordinates
 let y = Math.floor(width / 2);
@@ -275,7 +312,6 @@ let speed = 0.5;
 let lengthSnake = 1;
 const snakePositions = [snakeHead];
 snakePositions.push(snakeHead);
-console.log(snakePositions);
 let isEating = false;
 
 let applePosition = 0;
@@ -297,15 +333,12 @@ function showWalls() {
   wallsPositions.forEach(showWalls);
   function showWalls(cellNumber) {
     const cell = display[cellNumber];
-    console.log(cell);
-    console.log(cellNumber);
 
     cell.classList.add('wall');
   }
 }
 // Showing Pikachu on the cell with coordinates (posx, posy)
 function plotSnake() {
-  console.log(snakePositions[0]);
   const head = snakePositions[snakePositions.length - 1];
   const headCell = display[head];
   headCell.classList.remove('snakeHead');
@@ -322,14 +355,10 @@ function plotSnake() {
 }
 
 function showSnake(array) {
-  console.log('accessing showsnake');
-  console.log(array);
   array.forEach(showBody);
   function showBody(cellNumber) {
     const cell = display[cellNumber];
     if (cellNumber === array[array.length - 1]) {
-      console.log(array);
-      console.log(cell);
       cell.classList.add('snakeHead');
     } else {
       cell.classList.add('snakeBody');
@@ -404,7 +433,6 @@ function handleKeyPress(event) {
 
       break;
     default:
-      console.log('Not an arrow');
+      break;
   }
-  console.log(`(x,y)=${x},${y}`);
 }
