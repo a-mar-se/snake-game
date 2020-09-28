6f2a4747749588a06fbc284915482b8a70912210
a-mar-se <a.marquez.seco@hotmail.com>
2020-09-28 22:21:19 +0200

    structuring tutorial

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,12 @@
 // Initialize the program
 
 const startButton = document.querySelector('.startButton');
+const startButtonInf = document.querySelector('.startButtonInfinite');
 const startButtonContainer = document.querySelector('.startButtonContainer');
 function init() {
   // Click the button or press "Enter" or "SpaceBar" to start
   startButton.addEventListener('click', execGame);
+  startButtonInf.addEventListener('click', initInfinite);
   window.addEventListener('keydown', handleKeyPress);
 
   function handleKeyPress(event) {
@@ -16,6 +18,9 @@ function init() {
   }
   function execGame() {
     window.location.href = './level1-intro.html';
+  }
+  function initInfinite() {
+    window.location.href = './level1inf.html';
   }
 }
 
