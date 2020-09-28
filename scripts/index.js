df84eb9379cc7391fff975876e1e12f2a2d1ee31
a-mar-se <a.marquez.seco@hotmail.com>
2020-09-28 09:11:27 +0200

    Snake Moves when pressing an arrow

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,29 @@ function init() {
   console.log('Starting environment');
   const startButton = document.querySelector('.startButton');
 
-  startButton.addEventListener('click', initGame);
+  startButton.addEventListener('click', execGame);
+  function execGame() {
+    startButton.removeEventListener('click', initGame);
+    const winning = initGame();
+
+    console.log(winning);
+
+    if (winning) {
+      console.log('Wins!');
+    } else {
+      if (!winning) {
+        console.log('looosser!');
+      }
+    }
+  }
 }
 
+// init();
 document.addEventListener('DOMContentLoaded', init);
+
+// const startButton = document.createElement('button');
+// const startButtonContainer = document.createElement('div');
+// startButton.classList.add('startButton');
+// startButtonContainer.appendChild(startButton);
+// const doby = document.querySelector('body');
+// doby.appendChild(startButtonContainer);
